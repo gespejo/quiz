77b2bd700723571fa9a3c312153d9fbe1d4e6f69
Guillermo ESPEJO <gespejo@gmail.com>
2015-06-21 08:24:53 +0200

    quitar HEAD

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController,question);
-router.get('/quizes/answer', quizController,answer);
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
