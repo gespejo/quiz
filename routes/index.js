f5fdfef099bab1df166cc587b68df21a31f8a3ee
Guillermo ESPEJO <gespejo@gmail.com>
2015-07-09 08:13:33 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -33,7 +34,10 @@ router.get('/quizes/search',               quizController.search);
 router.get('/author', function(req, res) {
   res.render('credits', { title: 'Quiz', errors: []});
 });
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
