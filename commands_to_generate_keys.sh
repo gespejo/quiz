5929f9064b5cea587780e92c48ba2a757334955b
Guillermo Espejo <gespejo@gmail.com>
2015-07-10 05:57:14 +0100

    mover cert a certs

--- commands_to_generate_keys.sh
+++ commands_to_generate_keys.sh
@@ -2,5 +2,4 @@ mkdir cert
 cd cert
 openssl genrsa -out quiz-gespejo-key.pem 2048
 openssl req -new -sha256 -key quiz-gespejo-key.pem -out quiz-gespejo-csr.pem
-openssl x509 -req -in quiz-gespejo-csr.pem -signkey quiz-gespejo-key.pem -out
-quiz-gespejo-cert.pem
+openssl x509 -req -in quiz-gespejo-csr.pem -signkey quiz-gespejo-key.pem -out quiz-gespejo-cert.pem
