a7b695aafca899d92a321a059700335a9115217f
Guillermo ESPEJO <gespejo@gmail.com>
2015-07-10 06:45:00 +0200

    https

---
+++ commands_to_generate_keys.sh
@@ -0,0 +1,6 @@
+mkdir cert
+cd cert
+openssl genrsa -out quiz-gespejo-key.pem 2048
+openssl req -new -sha256 -key quiz-gespejo-key.pem -out quiz-gespejo-csr.pem
+openssl x509 -req -in quiz-gespejo-csr.pem -signkey quiz-gespejo-key.pem -out
+quiz-gespejo-cert.pem
